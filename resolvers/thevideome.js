dbd83b0e11bb15a2ae223cc6d5a13193d49ec7e6
c1ber <43691711+c1ber@users.noreply.github.com>
2018-12-27 22:40:13 -0500

    Update thevideome.js

--- resolvers/thevideome.js
+++ resolvers/thevideome.js
@@ -1,100 +1,85 @@
-// ==UserScript==
-// @name         thevideo.me
-// @namespace    Melkisedeh
-// @version      0.4
-// @description  try to take over the world!
-// @author       You
-// @match        https://thevideo.me/*
-// @match        https://openload.co/*
-// @match        https://gorillavid.in/* 
-// @match        https://daclips.in/* 
-// @match        http://allmyvideos.net/* 
-// @match        http://movpod.in/* 
-// @match        http://www.vodlocker.com/* 
-// @match        http://streamin.to/* 
-// @match        http://vidzi.nu/*  
-// @match        http://vidspot.net/* 
-// @match        http://vidto.me/* 
-// @match        http://vshare.eu/* 
-// @match        http://flashx.tv/*
-// @match        http://rosharing.net/*
-// @grant        none
-// ==/UserScript==
-/* jshint -W097 */
-'use strict';
+/* TheVideoDecoder - A script which will be executed by Duktape to extract TheVideo.me links
+ * 
+ * Copyright (C) 2017 NitroXenon
+ * 
+ * This software is released under the GPLv3 License.
 
-window.open = function (open) {
-    return function (url, name, features) {            
-        console.log(">>>>>> " + url);
-        //return open.call(window, url, name, features);
-    };
-}(window.open);
+Copyright (C) 2015 NitroXenon
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+var TheVideoDecoder = {
+    decode: function(url, html) {
+        Log.d("Start decoding in JS now...");
 
-function eventFire(el, etype){
-  if (el.fireEvent) {
-    el.fireEvent('on' + etype);
-  } else {
-    var evObj = document.createEvent('Events');
-    evObj.initEvent(etype, true, false);
-    el.dispatchEvent(evObj);
-  }
-}
+        var results = [];
 
-if(location.href.indexOf("https://openload.co/")>-1)
-{
-    eventFire(document.getElementById('videooverlay'), 'click');
-    if (window.self === window.top)
-        $(".vjs-big-play-button").click();
-    
-    //player.play();
-     //$("html").unbind('click');
-    function resetPP()
-    {
-       // $("html").unbind('click');
-        pos=1;
-        console.log('reset PP');    
-       // player.play();
+        var keyMatch = /thief\s*=\s*['"]([^'"]+?)['"]/g.exec(html);
+        
+        if (keyMatch == null)
+            return JSON.stringify(results);
+        
+        var key = "";
+        
+        if (keyMatch != null)
+            key = keyMatch[1];
+        
+        key = key.replace("+", "");
+        
+        var getVtLink = "https://thevideo.me/vsign/player/" + key;
+        var getVtLinkResult = Http.get(getVtLink, url);
+
+        Log.d("key = " + key);
+        Log.d("getVtLink = " + getVtLink);
+        Log.d("getVtLinkResult = " + getVtLinkResult);
+
+        var vtMatch = /\|([a-z0-9]{40}[a-z0-9]+?)\|/g.exec(getVtLinkResult);
+        if (vtMatch == null)
+            return JSON.stringify(results);
+
+        var vt = vtMatch[1];
+
+        var linkMatches = getMatches(html, /"file"\s*:\s*"([^"]+)"\s*,\s*"label"\s*:\s*"([^"]+)/g, 1);
+        var qualityMatches = getMatches(html, /"file"\s*:\s*"([^"]+)"\s*,\s*"label"\s*:\s*"([^"]+)/g, 2);
+
+        for (var i = 0; i < linkMatches.length; i++) {
+            try {
+                var link = linkMatches[i] + "?direct=false&ua=1&vt=" + vt;
+                var quality = qualityMatches[i];
+
+                Log.d("link = " + link);
+                Log.d("quality = " + quality);
+
+                results.push({
+                    quality: quality,
+                    link: link
+                });
+            } catch (err) {
+                Log.d(err.message);
+            }
+        }
+
+        return JSON.stringify(results);
+    },
+    isEnabled: function() {
+        return true;
     }
-    //window.resetPP = resetPP;
-    //window.setTimeout("window.resetPP();", 2000);
-}
-else if(location.href.indexOf("https://thevideo.me/")>-1)
-{
-    if(location.href.indexOf("embed")==-1)
-        location.href = location.href.replace("https://thevideo.me/", "https://thevideo.me/embed-") + "-640x360.html"
+};
 
-        $("#veriform").submit();
-    $(".player_hover_a").hide();
-    $("#fci").hide();
-
-    function obsoletefn() {console.log('obsoletefn');}
-    function resetPP()
-    {
-        window.jsPopunder=obsoletefn;
-        window.check_pop=obsoletefn;
-        window.install_repeat_popup=obsoletefn;
-        window.install_popads_popup=obsoletefn;
-        window.install_2nd_popup=obsoletefn;
-
-        jwplayer().onPause(function(){
-            console.log('jwplayer pause');    
-        });
+function getMatches(string, regex, index) {
+    index || (index = 1); // default to the first capturing group
+    var matches = [];
+    var match;
+    while (match = regex.exec(string)) {
+        matches.push(match[index]);
     }
-    window.resetPP = resetPP;
-    window.clearObject = clearObject;
-
-    function clearObject()
-    {
-        var bfa = $("#beforeswfanchor0")[0];
-        if(bfa==null)
-            return;
-        var ref = bfa.href;
-        ref = ref.split("#")[1];
-        $("#"+ref).remove();
-    }
-
-    window.setTimeout("window.resetPP();window.clearObject();", 2000);
-    
-    if (window.self === window.top)
-        jwplayer().play();
+    return matches;
 }
